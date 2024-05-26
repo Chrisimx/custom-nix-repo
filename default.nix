148e464c2efac2e8bcd8f89dd957d82df3729c7c
Christian <chris.imx@online.de>
2024-05-26 21:57:13 +0200

    Deactivate example-package

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   vosk = pkgs.callPackage ./pkgs/vosk { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
